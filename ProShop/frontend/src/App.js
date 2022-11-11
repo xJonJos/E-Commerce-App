4f8b2c44f7d6ae1340f69d21ce23a1a20fa52c1d
Jon-Jos <jonthaiparambil@gmail.com>
2022-11-11 22:05:14 +0530

    first commit

---
+++ ProShop/frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
